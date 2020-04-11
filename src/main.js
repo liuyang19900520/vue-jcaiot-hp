9fb983cc1dcb4b90d357db0da141e577b7d751d3
liuyang19900520 <liuyang19900520@hotmail.com>
2020-04-11 23:50:40 +0900

    vue post view

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import i18n from './plugins/i18n';
 import router from './router/';
 import store from './store';
 import api from './api'
-
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 
 Vue.prototype.$api = api; // 将api挂载到vue的原型上
 Vue.config.productionTip = false;
@@ -15,5 +16,6 @@ new Vue({
     i18n,
     router,
     store,
+    mavonEditor,
     render: h => h(App)
 }).$mount('#app');
--- src/views/pc/web/PostView.vue
+++ src/views/pc/web/PostView.vue
@@ -1,40 +1,42 @@
 <template>
-    <div>
-        <div dense class="center80">
-            <h2>{{item.title}}</h2>
+    <div class="center80">
 
-
-        </div>
-
+        <h1 class="center">刘社长出系我们的2020年的尚颖慧</h1>
+        <br/>
+        <v-img src="https://picsum.photos/510/300?random" aspect-ratio="3"></v-img>
+        <br/>
+        <mavon-editor v-model="mkValue"/>
 
     </div>
 </template>
 
 <script>
+
     export default {
-        name: "PostView",
-        data: () => ({
-            page: 1,
-            item:
-                {
-                    src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
-                    title: '刘劲博士组织2020年IOT协会赏樱会',
-                    date: '2020/04/09',
-                    summary: 'Java 是由Sun Microsystems公司于1995年5月推出的高级程序设计语言。Java可运行于多个平台，如Windows, Mac' +
-                        'OS，及其他多种U NIX版本的系统。本教程通过简单的实例将让大家更好的了解JAVA编程语言。'
-                }
-        }),
-    }
+
+        props: {
+            source: String
+        },
+        data: () => ({mkValue: null,}),
+        created() {
+
+        },
+        methods: {},
+        watch: {
+            "$route.path": "getBanner"
+        }
+    };
 </script>
 
 <style scoped>
     .center80 {
-        margin-left: 10%;
-        margin-right: 10%;
+        margin-left: 15%;
+        margin-right: 15%;
     }
 
-    .item-margin {
-        margin-bottom: 3%;
+    .center {
+        text-align: center;
+        margin: 2% auto;
     }
 
 
